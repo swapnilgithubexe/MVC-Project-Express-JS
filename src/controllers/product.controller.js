44bd239a3e8d1b08da59901e53c16fc893a298fd
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-09-09 23:18:47 +0530

    updates

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,7 +1,11 @@
 import path from "path";
+import ProductModel from "../models/product.model.js";
+import { log } from "console";
 
 export default class ProductController {
   getProducts(req, res) {
+    let products = ProductModel.get()
+
     return res.sendFile(path.join(path.resolve(), "src", "views", "products.html"))
   }
 }
