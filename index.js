2b32a1874923a673d7497bea8894fc902fff6cca
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-09-10 23:18:34 +0530

    updates

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import ProductController from "./src/controllers/product.controller.js";
+import path from "path";
 
 const server = express();
+
+server.set("view engine", "ejs")
+server.set("views", path.join(path.resolve(), "src", "views"))
 const productController = new ProductController();
 
 server.get("/", productController.getProducts);
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,12 +1,12 @@
 import path from "path";
 import ProductModel from "../models/product.model.js";
-import { log } from "console";
 
 export default class ProductController {
   getProducts(req, res) {
     let products = ProductModel.get()
 
-    return res.sendFile(path.join(path.resolve(), "src", "views", "products.html"))
+    res.render("products", { products: products })
+    // return res.sendFile(path.join(path.resolve(), "src", "views", "products.html"))
   }
 }
 
