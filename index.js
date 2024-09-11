e3b5154da94f940acb5d54a65e7e8aef83bc0531
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-09-11 22:48:12 +0530

    updates

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import ProductController from "./src/controllers/product.controller.js";
 import path from "path";
+import ejsLayouts from "express-ejs-layouts";
 
 const server = express();
 
 server.set("view engine", "ejs")
 server.set("views", path.join(path.resolve(), "src", "views"))
+server.use(ejsLayouts);
+
 const productController = new ProductController();
 
 server.get("/", productController.getProducts);
