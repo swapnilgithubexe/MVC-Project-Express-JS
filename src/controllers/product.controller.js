baabdcfac5eca677f022b53fc3554daf507169f3
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-09-12 21:57:02 +0530

    added server side validation

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,10 +10,28 @@ export default class ProductController {
   }
 
   getAddForm(req, res) {
-    return res.render("new-product")
+    return res.render("new-product", { errorMessage: null })
   }
 
   addNewProduct(req, res) {
+    const { name, price, imageUrl } = req.body;
+    let errors = [];
+    if (!name || name.trim() === "") {
+      errors.push("Name is required");
+    }
+    if (!price || parseFloat(price) < 1) {
+      errors.push("Price should be a positive number");
+    }
+    try {
+      const validUrl = new URL(imageUrl);
+    } catch (error) {
+      errors.push("Invalid URL");
+    }
+
+    if (errors.length > 0) {
+      return res.render("new-product", { errorMessage: errors[0] })
+    }
+
     ProductModel.add(req.body)
     let products = ProductModel.get()
     res.render('products', { products: products })
